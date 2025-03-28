83489f22e8249f7968d177ece50d3f9cd91f0713
DawnteaStudio <dawnteabro@naver.com>
2025-03-28 20:52:17 +0900

    Feat: custom logger 추가와 내장 logger 조건 변경

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,9 @@ import Fastify from 'fastify';
 import { config } from './config';
 import { registerRoutes } from './routes';
 import { init } from './startup';
-// import { } from './lib';
+import { logger } from './lib';
 
-const app = Fastify({ logger: true });
+const app = Fastify({ logger: { level: 'error' } });
 
 const startServer = async () => {
   try {
@@ -16,9 +16,9 @@ const startServer = async () => {
 
     // 서버 시작
     await app.listen({ port: config.port, host: '127.0.0.1' });
-    app.log.info(`Server is running on port ${config.port}`);
+    logger.log(`Server is running on port ${config.port}`);
   } catch (error) {
-    app.log.error(`Error starting server: ${error}`);
+    logger.error(`Error starting server: ${error}`);
     process.exit(1);
   }
 };
