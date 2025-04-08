b7fd0c4267778dbe097424febe5760021894f18e
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2025-04-08 16:50:20 +0900

    #37 [BE] 초기 폴더구성과 indexts 설정 (#39)
    * Feat: main.ts 및 각 폴더별 index.ts 구성, 초기 Prisma 마이그레이션 적용
    
    * Feat: custom logger 추가와 내장 logger 조건 변경
    
    * Feat: schema/type/index.ts 파일 추가

---
+++ backend/src/lib/logger.ts
@@ -0,0 +1,4 @@
+export const logger = {
+  log: (msg: string) => console.log(`[LOG] ${msg}`),
+  error: (msg: string) => console.error(`[ERROR] ${msg}`),
+};
---
+++ backend/src/schema/type/index.ts
@@ -0,0 +1,2 @@
+import { Static } from '@sinclair/typebox';
+import * as schema from '../';
