10f62dd201053183a87ab4871e11839a772963c0
DawnteaStudio <dawnteabro@naver.com>
2025-04-17 13:18:23 +0900

    Remove: cookie test 함수 삭제

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -10,29 +10,6 @@ export async function loginRoutes(fastify: FastifyInstance) {
 
 export async function authRoutes(fastify: FastifyInstance) {
   fastify.get('/google/callback', handler.googleCallbackHandler);
-  fastify.get('/test/cookie', async (req, reply) => {
-    const dummyToken = 'dummy_token_123';
-    const dummyUserId = 1; // ⚠️ 테스트용으로 미리 DB에 존재하는 유저 ID를 사용해야 함!
-
-    // DB에 저장
-    await prisma.refreshToken.create({
-      data: {
-        userId: dummyUserId,
-        token: dummyToken,
-      },
-    });
-
-    // 쿠키로 내려보내기
-    reply
-      .setCookie('refreshToken', dummyToken, {
-        httpOnly: true,
-        secure: false,
-        sameSite: 'strict',
-        path: '/',
-        maxAge: 60 * 60 * 24 * 7,
-      })
-      .send({ message: '테스트용 쿠키와 DB 등록 완료' });
-  });
 }
 
 export async function refreshRoutes(fastify: FastifyInstance) {
