2c98f9054f0872a7df79002bc41c9cf3a0060cb8
DawnteaStudio <dawnteabro@naver.com>
2025-04-24 04:38:56 +0900

    Feat: 스키마 추가와 test state 롤백

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -3,7 +3,7 @@ import * as handler from '../handlers/auth.handler';
 import { loginSchema, refreshSchema, logoutSchema } from '../schema';
 
 const authRoute = async (fastify: FastifyInstance): Promise<void> => {
-  fastify.get('/login', { schema: loginSchema }, handler.loginRedirectHandler);
+  fastify.get('/login', handler.loginRedirectHandler);
   fastify.get('/google/callback', handler.googleCallbackHandler);
   fastify.post('/refresh', { schema: refreshSchema }, handler.refreshHandler);
   fastify.get('/logout', { schema: logoutSchema }, handler.logoutHandler);
