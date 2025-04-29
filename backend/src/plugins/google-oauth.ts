66022e901acda0b550d437ce30d0da9c06b0a97e
jeayoungKim529 <jeay529@gmail.com>
2025-04-29 17:30:29 +0900

    fix : login 부분 수정

--- backend/src/plugins/google-oauth.ts
+++ backend/src/plugins/google-oauth.ts
@@ -14,8 +14,6 @@ export default fp(async (fastify) => {
       auth: fastifyOauth2.GOOGLE_CONFIGURATION,
     },
     startRedirectPath: '/auth/google',
-    callbackUri: env.googleCallbackUrl,
-    generateStateFunction: () => 'test-state', // 테스트 끝나면 반드시 제거
-    checkStateFunction: () => true, // 테스트 끝나면 반드시 제거
+    callbackUri: env.googleCallbackUrl
   });
 });
