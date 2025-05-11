70b9671086e054cab395543a9dafb66280738e75
DawnteaStudio <dawnteabro@naver.com>
2025-05-11 22:25:04 +0900

    Feat: login 라우터 스키마 누락 수정

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -3,7 +3,7 @@ import { authHandler } from '../handlers';
 import { loginSchema, refreshSchema, logoutSchema } from '../schema';
 
 const authRoute = async (fastify: FastifyInstance): Promise<void> => {
-  fastify.get('/login', authHandler.login);
+  fastify.get('/login', { schema: loginSchema }, authHandler.login);
   fastify.get('/google/callback', authHandler.googleCallback);
   fastify.post('/refresh', { schema: refreshSchema }, authHandler.refresh);
   fastify.get('/logout', { schema: logoutSchema }, authHandler.logout);
