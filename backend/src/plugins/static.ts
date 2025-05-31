83029f36f7df5691a4f62a8b24845a6daf180eda
DawnteaStudio <dawnteabro@naver.com>
2025-05-31 21:50:26 +0900

    Fix: static es 방식으로 수정

--- backend/src/plugins/static.ts
+++ backend/src/plugins/static.ts
@@ -1,7 +1,11 @@
 import fp from 'fastify-plugin';
 import fastifyStatic from '@fastify/static';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { env } from '../config/env';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default fp(async (fastify) => {
   fastify.register(fastifyStatic, {
