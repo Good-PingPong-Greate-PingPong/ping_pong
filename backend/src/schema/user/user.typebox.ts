8b6195e3ea81f7f4b12f0426d99494ccc8d6b1b3
DawnteaStudio <dawnteabro@naver.com>
2025-06-01 01:09:38 +0900

    Fix: form으로 받지 않고 body로 받도록 설정한 스키마 수정

--- backend/src/schema/user/user.typebox.ts
+++ backend/src/schema/user/user.typebox.ts
@@ -2,18 +2,6 @@
 import { Type } from '@sinclair/typebox';
 
 /**
- * 🔹 유저 프로필 업데이트 요청 스키마
- */
-const TUpdateUserProfileRequest = Type.Partial(
-  Type.Object({
-    nickname: Type.String({ minLength: 1, maxLength: 30 }),
-    email: Type.String({ format: 'email' }),
-    twoFactorEnabled: Type.Boolean(),
-    profileImage: Type.String(), // 파일 URL 문자열 (파일 업로드 후 저장되는 경로)
-  }),
-);
-
-/**
  * 🔹 유저 프로필 업데이트 성공 응답 스키마
  */
 const TUpdateUserProfileResponse = Type.Object({
@@ -29,7 +17,4 @@ const TUpdateUserProfileResponse = Type.Object({
   }),
 });
 
-export {
-  TUpdateUserProfileRequest,
-  TUpdateUserProfileResponse,
-};
+export { TUpdateUserProfileResponse };
