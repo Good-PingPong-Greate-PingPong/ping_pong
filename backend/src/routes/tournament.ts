227adb07e2c6b0066b31647f90336e1bd7c0ecbe
jeayoungKim529 <jeay529@gmail.com>
2025-06-19 19:38:26 +0900

    feat: route

---
+++ backend/src/routes/tournament.ts
@@ -0,0 +1,18 @@
+import { FastifyInstance } from 'fastify';
+import { jwtUtil } from '../lib';
+import { readTournamentSchema } from '../schema';
+import { ListQueryRoute } from '../schema/type/index';
+import { tournamentHandler } from '../handlers/index';
+
+const tournamentRoute = async (fastify: FastifyInstance) => {
+  const tokenType = jwtUtil.tokenTypes;
+
+  fastify.get<ListQueryRoute>(
+    '/results',
+    {
+      schema: readTournamentSchema,
+      preHandler: jwtUtil.verifyTokenPreHandler(tokenType.access),
+    },
+    tournamentHandler.readLocalGame,
+  );
+};
