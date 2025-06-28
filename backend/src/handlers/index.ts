08083b74ea34ae3d1aedab89d46c2cbd68124246
DawnteaStudio <dawnteabro@naver.com>
2025-06-29 04:40:58 +0900

    Feat: websocket 핸들러 추가

--- backend/src/handlers/index.ts
+++ backend/src/handlers/index.ts
@@ -1,3 +1,4 @@
 export { default as authHandler } from './auth.handler';
 export { default as twoFAHandler } from './2fa.handler';
 export { default as userHandler } from './user.handler';
+export { default as websocketHandler } from './websocket.handler';
