1b1cd0746993805531690a531927505e95a45c82
jea0529 <80964104+jeayoungKim529@users.noreply.github.com>
2025-06-29 13:27:33 +0900

    #12 [BE] 승패 기록 조회 api 구현 (#57)
    * feat: type 및 schema 정의
    
    * feat: service 구현
    
    * feat: router
    
    * feat: handler
    
    * fix: type 수정
    
    * fix:
    
    * fix: type 오류 수정
    
    * fix:
    
    * feat : handler index
    
    * fix : prettier
    
    * fix : type명 대문자로 수정
    
    * fix: 경로 수정
    
    * fix: camelCase
    
    * fix: user not found error 추가

--- backend/src/handlers/localgame.handler.ts
+++ backend/src/handlers/localgame.handler.ts
@@ -2,9 +2,13 @@ import { FastifyReply, FastifyRequest } from 'fastify';
 import { ERROR_MESSAGE, SUCCESS_MESSAGE } from '../lib/constants';
 import { handlerUtil } from '../lib';
 import localGameService from '../services/localgame.service';
+import { CreateLocalGameRoute, ListQueryRoute } from '../schema/type/index';
 
 const localGameHandler = () => {
-  const createLocalGame = async (req: FastifyRequest, rep: FastifyReply) => {
+  const createLocalGame = async (
+    req: FastifyRequest<CreateLocalGameRoute>,
+    rep: FastifyReply,
+  ) => {
     try {
       await localGameService.createLocalGame(req.body, req.user.userId); // req.body에서 오류발생
       handlerUtil.handleSuccess(rep, SUCCESS_MESSAGE.registerOK);
@@ -13,8 +17,27 @@ const localGameHandler = () => {
     }
   };
 
+  const readLocalGame = async (
+    req: FastifyRequest<ListQueryRoute>,
+    rep: FastifyReply,
+  ) => {
+    try {
+      const page = Number(req.query.page);
+      const offset = Number(req.query.offset);
+      const result = await localGameService.readLocalGame(
+        req.user.userId,
+        page,
+        offset,
+      );
+      handlerUtil.handleSuccess(rep, SUCCESS_MESSAGE.registerOK, result);
+    } catch (error) {
+      handlerUtil.handleError(rep, ERROR_MESSAGE.badRequest, error);
+    }
+  };
+
   return {
     createLocalGame,
+    readLocalGame,
   };
 };
 
