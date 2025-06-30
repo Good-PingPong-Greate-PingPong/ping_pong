c3c62e7d2a619d3af025cae4aeacf35bd56a5065
Tea Green <90132183+DawnteaStudio@users.noreply.github.com>
2025-06-30 10:10:12 +0900

    #24 [BE} 유저정보 조회 api 구현 (#63)
    * Feat: websocket pkg 설치
    
    * Feat: websocket plugin 추가
    
    * Feat: app에 websocket 등록
    
    * Feat: 유저 닉네임 조회 라우터 추가
    
    * Refactor: schema import문 리팩토링
    
    * Feat: 닉네임 조회 핸들러 추가
    
    * Refactor: schema index 파일 별칭 추가
    
    * Feat: 유저 검색 핸들러 추가
    
    * Feat: 유저 검색 라우터 추가
    
    * Fix: schema import 롤백
    
    * Feat: 유저 목록 서비스로직 추가
    
    * Fix: 중복 코드 제거
    
    * Fix: 불필요한 중복 로직 제거
    
    * Feat: websocket 핸들러 추가
    
    * Feat: 웹소켓 온라인 유저관리 lib 추가
    
    * Fix: jwt 로직 웹소켓과 호환 위해 분리
    
    * Feat: webSocketQuery 타입 추가
    
    * Feat: 웹소켓 router 추가
    
    * Feat: 유저 목록 서비스 로직에 웹소켓 사용 함수 추가
    
    * Feat: 유저 목록 타입, 스키마 추가

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,12 +3,14 @@ import authRoute from './auth';
 import localGameRoute from './localgame';
 import twoFARoute from './2fa';
 import userRoute from './user';
+import websocketRoute from './websocket';
 
 const routes = async (fastify: FastifyInstance): Promise<void> => {
   fastify.register(authRoute, { prefix: '/api/auth' });
   fastify.register(twoFARoute, { prefix: '/api/2fa' });
   fastify.register(userRoute, { prefix: '/api/users' });
   fastify.register(localGameRoute, { prefix: '/api/localgame' });
+  fastify.register(websocketRoute);
 };
 
 export default routes;
