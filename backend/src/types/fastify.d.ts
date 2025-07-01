6632d261dcecfe93a0e491e3a5822a66e0d08588
DawnteaStudio <dawnteabro@naver.com>
2025-07-01 21:21:44 +0900

    Fix: fastify.d.ts 하나의 모듈에 interface 통합

--- backend/src/types/fastify.d.ts
+++ backend/src/types/fastify.d.ts
@@ -2,28 +2,37 @@ import 'fastify';
 import { PrismaClient } from '@prisma/client';
 import { OAuth2Namespace } from '@fastify/oauth2';
 import { Multipart, MultipartFile } from '@fastify/multipart';
+import { JwtPayload } from 'jsonwebtoken';
 
 declare module 'fastify' {
   interface FastifyInstance {
     prisma: PrismaClient;
     googleOAuth2: OAuth2Namespace;
   }
-}
 
-declare module 'fastify' {
+  // ✅ 친구 목록 요청용 타입
+  interface FriendListQuery {
+    Querystring: {
+      page?: number;
+    };
+  }
+
+  // ✅ 친구 추가/삭제 요청용 타입
+  interface ModifyFriendBody {
+    Body: {
+      receiverId: number;
+    };
+  }
+
   interface FastifyRequest {
     user: TokenPayload;
     isMultipart: () => boolean;
   }
-}
-import { JwtPayload } from 'jsonwebtoken';
-declare module 'fastify' {
+
   interface TokenPayload extends JwtPayload {
     userId: number;
   }
-}
 
-declare module 'fastify' {
   interface WebSocketQuery {
     Querystring: {
       token: string;
