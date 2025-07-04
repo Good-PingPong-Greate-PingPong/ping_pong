e662374474e8b583c4169c1e007d09a42b586869
jea0529 <80964104+jeayoungKim529@users.noreply.github.com>
2025-07-04 15:20:45 +0900

    #16 [BE] 토너먼트 전체 결과 조회 api 구현 (#59)
    * feat: schema
    
    * feat: handler
    
    * feat: route
    
    * feat: services

---
+++ backend/src/handlers/tournament.handler.ts
@@ -0,0 +1,25 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { ERROR_MESSAGE, SUCCESS_MESSAGE } from '../lib/constants';
+import { handlerUtil } from '../lib';
+import tournamentService from '../services/tournament.service';
+import { ListQueryRoute } from '../schema/type/index';
+
+const tournamentHandler = () => {
+  const readTournament = async (
+    req: FastifyRequest<ListQueryRoute>,
+    rep: FastifyReply,
+  ) => {
+    try {
+      const page = Number(req.query.page);
+      const result = await tournamentService.readTournament(
+        req.user.userId,
+        page,
+      );
+      handlerUtil.handleSuccess(rep, SUCCESS_MESSAGE.registerOK, result);
+    } catch (error) {
+      handlerUtil.handleError(rep, ERROR_MESSAGE.badRequest, error);
+    }
+  };
+
+  return { readTournament };
+};
